36bbc22b3bcd88fb111501fb125584fc093cabcc
evgenyelagin <anykeysky@gmail.com>
2020-12-10 20:48:25 +0300

    1.6. Взаимодействие объектов.[166087#293427] v.1

---
+++ src/main/java/ru/job4j/tracker/oop/Hare.java
@@ -0,0 +1,7 @@
+package ru.job4j.tracker.oop;
+
+public class Hare {
+    public void tryEat(Ball ball) {
+        System.out.println("Колобок, колобок - я тебя съем, я кровожадный Заяц");
+    }
+}
