b41e7d3834d24e59e0975c92e15330be369df7b4
evgenyelagin <anykeysky@gmail.com>
2020-12-10 20:47:57 +0300

    1.6. Взаимодействие объектов.[166087#293427] ball

---
+++ src/main/java/ru/job4j/tracker/oop/Ball.java
@@ -0,0 +1,9 @@
+package ru.job4j.tracker.oop;
+
+public class Ball {
+    public void tryEat(Ball ball) {
+    }
+    public void goOutFromHome() {
+        System.out.println("Я Колобок, я устал - я ухожу в лес");
+    }
+}
