dd8934a7ce17be017679eb1dea9341474c6e08b3
evgenyelagin <anykeysky@gmail.com>
2021-01-13 14:17:25 +0300

    2. Перегрузка конструктора.[173890#293435]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,22 +1,23 @@
 package ru.job4j.tracker;
 
 public class Item {
-    private int id;
-    private String name;
 
-    public int getId() {
-        return id;
+    private String name;
+    private int id;
+
+    public Item() {
     }
 
-    public void setId(int id) {
+    public Item(int id) {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public Item(String name, int id) {
+        this.name = name;
+        this.id = id;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public static void main(String[] args) {
+        Item item = new Item();
     }
 }
