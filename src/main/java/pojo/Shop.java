f9d492c45fd29851ce254f1f0f0d4e46a4f21b59
evgenyelagin <anykeysky@gmail.com>
2021-01-30 22:26:59 +0300

    3. Удаление моделей из массива.[176470#293444]

---
+++ src/main/java/pojo/Shop.java
@@ -0,0 +1,14 @@
+package pojo;
+
+public class Shop {
+    public static int indexOfNull(Product[] products) {
+        int rsl = -1;
+        for (int i = 0; i < products.length; i++) {
+            if (products[i] == null) {
+                rsl = i;
+                break;
+            }
+        }
+        return rsl;
+    }
+}
