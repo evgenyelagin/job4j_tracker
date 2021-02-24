ff22466d20ef0b988fdddce1885ec984ce8fcce0
evgenyelagin <anykeysky@gmail.com>
2021-02-24 09:51:16 +0300

    5. Tracker - хранилище[396#293448]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,24 +1,50 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
     private int size = 0;
 
-    public Item add(Item point) {
-        point.setId(ids++);
-        items[size++] = point;
-        return point;
+    public Item add(Item item) {
+        item.setId(ids++);
+        items[size++] = item;
+        return item;
     }
 
     public Item findById(int id) {
         Item rsl = null;
         for (int index = 0; index < size; index++) {
-            Item element = items[index];
-            if (element.getId() == id) {
-                rsl = element;
+            Item item = items[index];
+            if (item.getId() == id) {
+                rsl = item;
                 break;
             }
+        }
+        return rsl;
+    }
+
+    public Item[] findAll() {
+        Item[] namesWithoutNull = new Item[items.length];
+        int count = 0;
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] != null) {
+                namesWithoutNull[count] = items[i];
+                count++;
+            }
+        }
+        return Arrays.copyOf(namesWithoutNull, count);
+    }
+
+    public Item findByName(String key) {
+        Item rsl = null;
+        int size = 0;
+        for (int i = 0; i < items.length; i++) {
+            Item id = items[i];
+            if (id.equals(key));
+            rsl = id;
+            size++;
         }
         return rsl;
     }
